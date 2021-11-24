1515cee80d342b3609473dc82aff95e54c3b5b96
Antonin Lesoudier <lesoudier.antonin@gmail.com>
2021-11-25 00:07:39 +0100

    Reworked the game StateMachine to be more performance-friendly

--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -9,16 +9,15 @@ public class GlobalManager : MonoBehaviour
 
     public List<ActionInGame> ListActionsInGame = new List<ActionInGame>();
 
-    private EnumClass.GameState gameState;
+    private EnumClass.GameState currentGameState;
 
-    [SerializeField] private float waitingForActionTimerDuration;
-    [SerializeField] private float waitingForBuffChoiceDuration;
-    private float timer = 0;
+    [SerializeField] private int actionsTimerDuration;
+    [SerializeField] private int buffTimerDuration;
+    private int currentTimer;
 
+    [SerializeField] private int[] buffChoiceTurns;
     private int turnCount;
 
-    private bool currentlyPlayingActions;
-    
     private void Awake()
     {
         if (Instance == null)
@@ -33,56 +32,9 @@ public class GlobalManager : MonoBehaviour
 
     private void Start()
     {
-        gameState = EnumClass.GameState.WaitingTurn;
+        StartState(EnumClass.GameState.WaitingTurn);
     }
 
-    private void Update()
-    {
-        HandleGameStates(Time.deltaTime);
-    }
-
-
-    void HandleGameStates(float deltaTime)
-    {
-        switch (gameState)
-        {
-            case EnumClass.GameState.WaitingTurn:
-
-                timer += Time.deltaTime;
-
-                if (timer >= waitingForActionTimerDuration)
-                {
-                    SwitchToState(EnumClass.GameState.ActionTurn);
-                    ResetTimer();
-                }
-                
-                break;
-            
-            case EnumClass.GameState.ActionTurn:
-                StartAllActionsInGame();
-                break;
-            
-            case EnumClass.GameState.ChoseBuffTurn:
-
-                //UIManager.Instance.DisplayBuffChoices(turnCount);
-                
-                timer += Time.deltaTime;
-
-                if (timer >= waitingForBuffChoiceDuration)
-                {
-                    SwitchToState(EnumClass.GameState.WaitingTurn);
-                    ResetTimer();
-                }
-                
-                break;
-            
-            case EnumClass.GameState.GameEnd:
-
-                break;
-            
-        }
-    }
-    
     public void AddActionInGameToList(ActionInGame ActionToAdd)
     {
         //Ici on devra trier si le propri�taire de l'action que l'on ajoute a la liste n'avait pas deja une action dans la liste avant de remettre son action. 
@@ -92,30 +44,100 @@ public class GlobalManager : MonoBehaviour
 
     public void StartAllActionsInGame()
     {
-        currentlyPlayingActions = true;
-        ListActionsInGame[0].LaunchActionInGame();
+        if (ListActionsInGame.Count > 0)
+        {
+            ListActionsInGame[0].LaunchActionInGame();
+        }
+        else
+        {
+            EndActionTurn();
+        }
+        
     }
+    
 
-    public void ResetTimer()
+    void StartState(EnumClass.GameState nextState)
     {
-        timer = 0;
+        currentGameState = nextState;
+        
+        switch (currentGameState)
+        {
+            case(EnumClass.GameState.WaitingTurn):
+                StartCoroutine(ActionsChoiceCoroutine());
+                break;
+            
+            case(EnumClass.GameState.ActionTurn):
+                StartAllActionsInGame();
+                break;
+            
+            case(EnumClass.GameState.ChoseBuffTurn):
+                StartCoroutine(BuffChoiceCoroutine());
+                break;
+            
+            case(EnumClass.GameState.GameEnd):
+                //Display End Screen
+                break;
+        }
     }
 
-    void SwitchToState(EnumClass.GameState nextState)
+    IEnumerator ActionsChoiceCoroutine()
     {
-        gameState = nextState;
+        //Display Input Choice screen (and players life+name ?)
+        
+        currentTimer = actionsTimerDuration;
+
+        while (currentTimer > 0)
+        {
+            yield return new WaitForSeconds(1);
+            currentTimer--;
+            //Update ProgressBar
+        }
+        
+        //HideBuffScreen
+        StartState(EnumClass.GameState.ActionTurn);
+        
     }
 
+    IEnumerator BuffChoiceCoroutine()
+    {
+        //SetBuffScreen
+        //DisplayBuffScreen
+
+        currentTimer = buffTimerDuration;
+
+        while (currentTimer > 0)
+        {
+            yield return new WaitForSeconds(1);
+            currentTimer--;
+            //Update ProgressBar
+        }
+        
+        //HideBuffScreen
+        StartState(EnumClass.GameState.WaitingTurn);
+    }
+    
     void EndActionTurn()
     {
-        currentlyPlayingActions = false;
         turnCount++;
+        //Update the UI turn counter
         CheckNextTurn(turnCount);
     }
 
     void CheckNextTurn(int nextTurn)
     {
+        if (buffChoiceTurns.Length > 0)
+        {
+            foreach (var buffChoiceTurn in buffChoiceTurns)
+            {
+                if (nextTurn == buffChoiceTurn)
+                {
+                    StartState(EnumClass.GameState.ChoseBuffTurn);
+                    return;
+                }
+            }
+        }
         
+        StartState(EnumClass.GameState.WaitingTurn);
     }
     
 }
