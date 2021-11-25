17eb3e463ee3f06d787934e469daaca3975c0bee
Antonin Lesoudier <lesoudier.antonin@gmail.com>
2021-11-25 15:47:46 +0100

    Added Character Animations

--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -42,6 +42,7 @@ public class GlobalManager : MonoBehaviour
     public void AddActionInGameToList(ActionInGame ActionToAdd)
     {
         //Ici on devra trier si le propri�taire de l'action que l'on ajoute a la liste n'avait pas deja une action dans la liste avant de remettre son action. 
+        
         Debug.Log(ActionToAdd + "have been added to the list of actions");
         ListActionsInGame.Add(ActionToAdd);
     }
