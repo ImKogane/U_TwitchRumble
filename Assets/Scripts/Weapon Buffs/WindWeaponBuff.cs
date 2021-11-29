7456c38b9759c07576a01e41bb7b36f2cc39d02a
Paulux92500 <paul.ray92500@gmail.com>
2021-11-29 17:07:47 +0100

    Update WindWeaponBuff.cs

--- Assets/Scripts/Weapon Buffs/WindWeaponBuff.cs
+++ Assets/Scripts/Weapon Buffs/WindWeaponBuff.cs
@@ -4,8 +4,52 @@ using UnityEngine;
 
 public class WindWeaponBuff : WeaponBuff
 {
-    public override void ApplyWeaponBuff()
+    public override void ApplyWeaponBuff(Player playerAffect, Player playerAttacking)
     {
+        if (playerAttacking.playerWeapon is HammerWeapon || playerAttacking.playerWeapon is RifleWeapon)
+        {
+            //Faux = a changer pour adapter correctement la rotation du joueur qui prend l'attaque. 
+            playerAffect.playerMovement.RotationOfPlayer = playerAttacking.playerMovement.RotationOfPlayer;
+            playerAffect.gameObject.transform.rotation = playerAttacking.gameObject.transform.rotation;
 
+            playerAffect.playerMovement.MakeMovement();
+        }
+        else if (playerAttacking.playerWeapon is ScytheWeapon)
+        {
+            int PosXAttacker = playerAttacking.CurrentTile.tileRow;
+            int PosYAttacker = playerAttacking.CurrentTile.tileColumn;
+
+            int PosXReceiver = playerAffect.CurrentTile.tileRow;
+            int PosYReceiver = playerAffect.CurrentTile.tileColumn;
+
+            if (PosXAttacker != PosXReceiver) // Defenseur a droite ou a gauche
+            {
+                if (PosXAttacker < PosXReceiver) // Gauche
+                {
+                    playerAffect.playerMovement.RotateRightDirection();
+                }
+                if(PosXAttacker > PosXReceiver) //Droite
+                {
+                    playerAffect.playerMovement.RotateLeftDirection();
+                }
+
+                playerAffect.playerMovement.MakeMovement();
+            }
+
+            if (PosYAttacker != PosYReceiver) // Defenseur en haut ou en bas
+            {
+                if (PosYAttacker < PosYReceiver) //Haut
+                {
+                    playerAffect.playerMovement.RotateUpDirection(); 
+                }
+                else if (PosYAttacker > PosYReceiver) //Bas
+                {
+                    playerAffect.playerMovement.RotateDownDirection(); 
+                }
+
+                playerAffect.playerMovement.MakeMovement();
+            }
+        }
+       
     }
 }
