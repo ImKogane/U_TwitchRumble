2244245125d731f30d10e82ad0930e459832c2d9
Paulux92500 <paul.ray92500@gmail.com>
2021-12-01 11:57:41 +0100

    text

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Weapon
 {
+    //Varaible SO weapon 
+
+    //Constructeur : prend en parametre ton SO weapon
+
     public virtual List<Tile> Attack(Vector2Int CurrentCellOfPlayer, Vector2Int RotationOfPlayer)
     {
         return null;
     }
-    
 }
