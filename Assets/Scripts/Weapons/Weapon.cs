0a648873f0f27a4611f471793f6503e863083f73
Antonin Lesoudier <lesoudier.antonin@gmail.com>
2021-12-02 15:38:01 +0100

    Tried to fix weapon prefab loading and SFX

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -17,9 +17,14 @@ public class Weapon
     {
         weaponData = DatasManager.Instance.GetWeaponData(weaponType);
         ownerPlayer = newOwnerPlayer;
-        ownerPlayer.playerAnimator.runtimeAnimatorController = weaponData.weaponAnimator;
-        Transform weaponSocket = ownerPlayer.transform.Find("Hand_R");
-        weaponPrefab = GameObject.Instantiate(weaponData.weaponPrefab, weaponSocket);
+        ownerPlayer.playerAnimator.runtimeAnimatorController = weaponData.weaponAnimatorController;
+        
+        foreach (Transform eachChild in ownerPlayer.transform) {
+            if (eachChild.CompareTag("WeaponSocket")) {
+                weaponPrefab = GameObject.Instantiate(weaponData.weaponPrefab, eachChild, false);
+            }
+        }
+        
     }
    
 
@@ -33,7 +38,7 @@ public class Weapon
     {
         foreach (var currentTransform in VFXtransformList)
         {
-            GameObject.Instantiate(weaponData.weaponVFX, currentTransform);
+            GameObject.Instantiate(weaponData.weaponVFX, currentTransform.position, Quaternion.identity); 
         }
         
         VFXtransformList.Clear();
