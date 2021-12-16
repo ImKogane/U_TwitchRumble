1fb2783f06ca868d0689a528c9398ee62a93742f
Paulux92500 <paul.ray92500@gmail.com>
2021-12-16 03:20:14 +0100

    Refacto 2 : Global Manager + Google Sheet Manager

--- Assets/Scripts/Managers/GlobalManager.cs
+++ Assets/Scripts/Managers/GlobalManager.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
 using Random = UnityEngine.Random;
 
--- Assets/Scripts/Managers/GoogleSheetManager.cs
+++ Assets/Scripts/Managers/GoogleSheetManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +5,8 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Services;
-using Google.Apis.Util.Store;
 using System;
 using System.IO;
-using System.Threading;
-using System.Linq;
-using System.Dynamic;
 using System.Threading.Tasks;
 
 public class GoogleSheetManager : SingletonMonobehaviour<GoogleSheetManager>
